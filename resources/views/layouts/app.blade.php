8b062057c15a7848a4b5941ba195a2118cb8f8c5
bola-cloud <bola.ishak41@gmail.com>
2024-12-25 12:04:13 +0200

    gallery

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -29,7 +29,7 @@
     <link rel="apple-touch-icon-precomposed" sizes="114x114" href="{{asset('ico/apple-touch-icon-114-precomposed.png')}}" />
     <link rel="apple-touch-icon-precomposed" sizes="72x72" href="{{asset('ico/apple-touch-icon-72-precomposed.png')}}" />
     <link rel="apple-touch-icon-precomposed" href="{{asset('ico/apple-touch-icon-57-precomposed.png')}}" />
-    <link rel="shortcut icon" href="{{asset('img/ictc.jpeg')}}" />
+    <link rel="shortcut icon" href="{{asset('img/461161.png')}}" />
 
     <style>
         

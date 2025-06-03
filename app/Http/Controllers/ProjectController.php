30ddf52120cba2ac45f2fd1d03784412610d06ef
bola-cloud <bola.ishak41@gmail.com>
2025-06-04 00:54:25 +0300

    projects

--- app/Http/Controllers/ProjectController.php
+++ app/Http/Controllers/ProjectController.php
@@ -5,14 +5,15 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\Project;
 use App\Models\Scope;
+use Illuminate\Support\Str;
 
 class ProjectController extends Controller
 {
     public function show($scopeSlug, $projectSlug)
     {
-        $scope = Scope::whereRaw("REGEXP_REPLACE(LOWER(en_title), '[^a-z0-9-]', '-') = ?", [$scopeSlug])->firstOrFail();
+        $scope = Scope::whereRaw("LOWER(?) = ?", [Str::slug($scopeSlug), Str::slug($scope->en_title)])->firstOrFail();
         $project = Project::where('scope_id', $scope->id)
-                         ->whereRaw("REGEXP_REPLACE(LOWER(en_name), '[^a-z0-9-]', '-') = ?", [$projectSlug])
+                         ->whereRaw("LOWER(?) = ?", [Str::slug($projectSlug), Str::slug($project->en_name)])
                          ->firstOrFail();
 
         return view('front.project-details', compact('project', 'scope'));
